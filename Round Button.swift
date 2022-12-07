2f922db23ec75b67b5e6ae28d3768fb15cd6a4b8
Bennett Mackenzie <90129223+scallywag89@users.noreply.github.com>
2022-12-07 22:59:18 +1100

    Round button class
    - Coded round button class to display changes in Storyboard

---
+++ Round Button.swift
@@ -0,0 +1,29 @@
+//
+//  Round Button.swift
+//  StopwatchClone
+//
+//  Created by Bennett Mackenzie on 7/12/2022.
+//
+
+import UIKit
+@IBDesignable
+class Round_Button: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
