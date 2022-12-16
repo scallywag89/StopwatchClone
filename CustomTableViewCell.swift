23d22d40a9806338de0d2bda567dfe0678ab5ac5
Bennett Mackenzie <90129223+scallywag89@users.noreply.github.com>
2022-12-17 10:25:08 +1100

    Create CustomTableViewCell.swift
    initial commit

---
+++ CustomTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.swift
+//  StopwatchClone
+//
+//  Created by Bennett Mackenzie on 17/12/2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    static let identifier = "CustomTableViewCell"
+}
